1fcf568558138285bf585c04c73fcc6175e27969
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-01-26 14:15:49 +0530

    Code Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(10)
 
+  const handleIncrease = () => {
+    setCount(count + 1)
+  }
+  
+  const handleDecrease = () => {
+    setCount(count - 1)
+  }
   return (
     <>
-      <div className='text-4xl bg-red-400'>
-      vfvkvksvkv
+      <div className='text-4xl h-screen bg-amber-50'>
+        <div className='flex justify-center '>Current count is {count}</div>
+        <div className='flex justify-center p-6'>
+        <button type='button' className='mr-4 cursor-pointer hover:text-5xl border-2 rounded-2xl px-3' onClick={handleIncrease}>Increase</button>       
+        <button type='button' className='ml-4 cursor-pointer hover:text-5xl border-2 rounded-2xl px-3' onClick={handleDecrease}>Decrease</button>       
+        <br /><br /></div>
       </div>
     </>
   )
---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+// src/vite-env.d.ts or vite-env.d.ts in the root (both work)
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly : string;  // Declare VITE_API as a string type
+  }
+  
+  interface ImportMeta {
+    readonly VITE_API: string;
+  }
+  
