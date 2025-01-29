11f3f28c2dec20228683e6f24a4b946d5e915e61
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-01-30 01:42:22 +0530

    App Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,103 @@
-import { useState } from 'react'
-function App() {
-  const [count, setCount] = useState(10)
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import gitlogo from "./image/gitlogo.png"
 
-  const handleIncrease = () => {
-    setCount(count + 1)
+function App() {
+  const [userOneName, setUserOneName] = useState<string>("")
+  const [userTwoName, setUserTwoName] = useState<string>("")
+  const [avatarOne, setAvatarOne] = useState<string>("")
+  const [avatarTwo, setAvatarTwo] = useState<string>("")
+
+
+  const Github_API = import.meta.env.VITE_GITHUB_API_TOKEN
+
+  const handleUserOne = (e: any) => {
+    setUserOneName(e.target.value)
   }
+
+  const handleUserTwo = (e: any) => {
+    setUserTwoName(e.target.value)
+  }
+
+  const userOne = async () => {
+    const obj1 = await axios.get(`https://api.github.com/users/${userOneName}`, {
+      headers: {
+        Authorization: `Bearer ${Github_API}`
+      }
+    })
+    const res = obj1.data.avatar_url
+    setAvatarOne(res)
+
+    const pub_repos = obj1.data.public_repos
+    const followers = obj1.data.followers
+  }
+
+  const userTwo = async () => {
+    const obj2 = await axios.get(`https://api.github.com/users/${userTwoName}`, {
+      headers: {
+        Authorization: `Bearer ${Github_API}`
+      }
+    })
+    const res = obj2.data.avatar_url
+    setAvatarTwo(res)
+
+    const pub_repos = obj2.data.public_repos
+    const followers = obj2.data.followers
+
+
+  }
+useEffect(() => {
+    if (!userOneName) return; // Don't fetch if inputs are empty
+    const delaySearch = setTimeout(() => {
+      userOne();
+    }, 500); // 1-second delay after user stops typing
+
+    return () => {
+      clearTimeout(delaySearch); // Cleanup timeout on each change
+    };
+  }, [userOneName, userTwoName]);
+
+  useEffect(() => {
+    if (!userTwoName) return; // Don't fetch if inputs are empty
+    const delaySearch = setTimeout(() => {
+      userTwo();
+    }, 500); // 1-second delay after user stops typing
+
+    return () => {
+      clearTimeout(delaySearch); // Cleanup timeout on each change
+    };
+  },[userTwoName])
   
-  const handleDecrease = () => {
-    setCount(count - 1)
-  }
   return (
     <>
-      <div className='text-4xl h-screen bg-amber-50'>
-        <div className='flex justify-center '>Current count is {count}</div>
-        <div className='flex justify-center p-6'>
-        <button type='button' className='mr-4 cursor-pointer hover:text-5xl border-2 rounded-2xl px-3' onClick={handleIncrease}>Increase</button>       
-        <button type='button' className='ml-4 cursor-pointer hover:text-5xl border-2 rounded-2xl px-3' onClick={handleDecrease}>Decrease</button>       
-        <br /><br /></div>
+      <div className="bg-[url('./image/bgimg.jpg')] bg-cover bg-center h-screen flex text-white text-center">
+        <div className='mt-12 bg-transparent h-58 w-full'>
+          <div className='h-56 bg-transparent mx-50 flex justify-center items-center'>
+            <div className="relative">
+              <img className="h-56 pr-36" src={gitlogo} alt="github logo" />
+              <img className="h-56 rounded-full absolute top-1/2 left-28 transform -translate-x-1/2 -translate-y-1/2 text-transparent" src={avatarOne} alt="user one image" />
+            </div>
+
+            <div className="relative ">
+              <img className="h-56 pl-36" src={gitlogo} alt="github logo" />
+              <img className="h-56  rounded-full absolute top-1/2 left-64 transform -translate-x-1/2 -translate-y-1/2 text- text-transparent" src={avatarTwo} alt="user two image" />
+            </div>
+
+
+          </div>
+          <div className='bg-transparent flex justify-center mt-6 h-12'>
+            <input className="bg-fuchsia-100 mr-31 border-black border-3 rounded-4xl text-black text-xl p-4 focus:outline-none focus:ring-0"
+              type='text'
+              value={userOneName}
+              onChange={handleUserOne} />
+            <input className="bg-fuchsia-100 ml-31 border-black border-3 rounded-4xl text-black text-xl p-4 focus:outline-none focus:ring-0"
+              type="text"
+              value={userTwoName}
+              onChange={handleUserTwo} />
+          </div>
+        </div>
       </div>
+
     </>
   )
 }
