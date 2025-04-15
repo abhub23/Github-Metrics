cbbf8c7691b73424181ed53f093e0b9044ad74a4
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-15 23:46:39 +0530

    fix: backend fixed

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,17 +1,19 @@
-import express from 'express'
-import cors from 'cors'
-import axios {isAxiosError} from 'axios';
 import dotenv from 'dotenv'
-import { isAxiosError } from 'axios'
-dotenv.config();
+dotenv.config({ path: '../../../.env.development' });
+import express from 'express'
+import type { Request, Response } from 'express';
+import cors from 'cors'
+import axios, {AxiosResponse} from 'axios';
 
 const PORT = 3001
-console.log(process.env.GITHUB_API_TOKEN)
+
   const Github_API = process.env.GITHUB_API_TOKEN
+  console.log(Github_API)
   const Gemini_API = process.env.GEMINI_API_TOKEN;
+  console.log(Gemini_API);
+  
   const Gemini_API_URL: string = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${Gemini_API}`;
 
-  console.log(Github_API)
 const app = express()
 app.use(cors({
     origin: "*"
@@ -19,25 +21,39 @@ app.use(cors({
 
 app.use(express.json())
 
-app.get('/api', async (req, res) => {
-    const data1 = req.query.data1
-    const data2 = req.query.data2
+type GeminiRequestBody = {
+    data1: string;
+    data2: string;
+  };
+
+app.post('/api', async (req: Request, res: any) => {
+    const {data1, data2} = req.body
+    console.log(data1, data2)
     try {
-        const data =  await axios.get(Gemini_API_URL, {
-            contents: [
-              {
-                parts: [
+        const response: AxiosResponse = await axios.post(Gemini_API_URL, 
+            {
+                contents: [
                   {
-                    text: `Compare these two users: ${data1} vs ${data2}. Roast the weaker one so bad that the user should feel ashamed of it and praise the one who have good repositories
-                     and lastly pick a winner. also dont give disclamer or any warning just pure roast. Give the output in about 150-180 words`,
+                    parts: [
+                      {
+                        text: `Compare these two users: ${data1} vs ${data2}. Roast the weaker one so bad that the user should feel ashamed of it and praise the one who have good repositories
+                         and lastly pick a winner. also dont give disclamer or any warning just pure roast. Give the output in about 150-180 words`,
+                      },
+                    ],
                   },
                 ],
-              },
-            ],
-          });
-    } catch (error) {
-        
-    }
+              }
+        )
+
+        const result: string = response.data.candidates?.[0]?.content?.parts?.[0]?.text || 'No response';
+        return res.status(200).send(result)
+    } catch (err: any) {
+        if (axios.isAxiosError(err)) {
+          console.error(`The error occured`, err.message);
+        }
+        console.error('Error occured in Gemini: ', err);
+        return;
+      }
 })
 
 app.listen(PORT, ()=> {
