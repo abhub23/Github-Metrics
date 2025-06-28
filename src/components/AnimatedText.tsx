90036911e52f3548caea94fb2df9e7fb09a5dbf9
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-06-28 16:13:46 +0530

    feat: animated text

---
+++ src/components/AnimatedText.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { motion, stagger, useAnimate } from 'motion/react';
+import { FC, useEffect } from 'react';
+
+type AnimateType = {
+  text: string;
+};
+
+const AnimatedText: FC<AnimateType> = ({ text }) => {
+  const [scope, animate] = useAnimate();
+
+  const AnimateText = () => {
+    animate(
+      'span',
+      {
+        opacity: 1,
+        filter: 'blur(0px)',
+        y: 0
+      },
+      {
+        duration: 0.3,
+        delay: stagger(0.01),
+        ease: "easeInOut"
+      }
+    );
+  };
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      AnimateText();
+    });
+  }, []);
+
+  return (
+    <div
+      ref={scope}
+      className="lg:h-56 h-[288px] mx-auto lg:w-[1400px] w-[320px] lg:p-4 lg:text-[16px] text-[13px] text-justify mt-6 text-black overflow-hidden"
+    >
+      {text.split('').map((word, idx) => (
+        <motion.span key={idx} initial={{ opacity: 0, y: 10 }}>
+          {word}
+        </motion.span>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedText;
